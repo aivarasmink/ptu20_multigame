62e8c3dd91b5da18c6b245cf779c0703e47222f9
aivarasmink <aivarasmink@gmail.com>
2024-03-11 15:34:27 +0200

    kuriu tetri galvojau bus lengviau kurti nei istikruju yra reikia labai daug isivaizdouti

---
+++ tetris_game/block.py
@@ -0,0 +1,17 @@
+from colors import Colors
+import pygame
+
+
+class Block:
+    def __init__(self, id):
+        self.id = id
+        self.cells = {}
+        self.cell_size = 30
+        self.rotation_state = 0
+        self.color = Colors.get_cell_colors()
+
+    def draw(self, screen):
+        tiles = self.cells[self.rotation_state]
+        for tile in tiles:
+            tile_rect = pygame.Rect(tile.column * self.cell_size + 1, tile.row * self.cell_size + 1, self.cell_size - 1, self.cell_size - 1)
+            pygame.draw.rect(screen, self.color[self.id], tile_rect)
